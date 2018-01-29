8abe4ef63e72bca805a8bcf2b0edd6d871205638
Moore.Huang <moore@moorehy.com>
2018-01-29 23:14:24 +0800

    F: 实现计算分期收益、费用、每期详细信息的功能函数

--- instalment/entry.py
+++ instalment/entry.py
@@ -3,6 +3,8 @@
 import sys
 import argparse
 
+import workflow
+
 
 def init_args_parser():
     des = u'''用例：
@@ -24,3 +26,26 @@ def init_args_parser():
         'fee', type=float, default=0, nargs='?',
         help=u'月分期费率，单位%，默认为0')
     return parser
+
+
+def calc_retained_profits(data):
+    rc = []
+    fee = 0
+    profits = data.cost
+    fee_per_stages = data.cost * data.fee / 100
+    for x in xrange(0, data.stages):
+        # 月初余额
+        profits_his = profits
+        # 月末余额
+        profits *= (1 + data.returns / 100) / 12
+        # 分期手续费
+        fee += fee_per_stages
+        # 填充列表
+        rc.append([x + 1, profits_his, profits, fee_per_stages])
+        # 偿还本金
+        profits -= data.cost / data.stages
+    rc.append([None, profits, profits, None])
+    print(u'收益：{}，费用：{}，净收益：{}'.format(
+        profits, fee, profits - fee))
+
+    return profits, fee, rc
