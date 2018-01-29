66de2eb7c879e82cd10fe5195fc6948aacee0976
Moore.Huang <moore@moorehy.com>
2018-01-29 17:51:49 +0800

    F: 实现命令行传参解析器初始化函数，用以生成参数解析器
    Signed-off-by: Moore.Huang <moore@moorehy.com>

--- instalment/entry.py
+++ instalment/entry.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import argparse
+
+
+def init_args_parser():
+    des = u'''用例：
+    ./entry.py 6000 12 4  # 将6000元分12期，年化收益率4%，月分期费率0%'''
+    parser = argparse.ArgumentParser(
+        prog='instalment',
+        description=des,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'cost', type=float,
+        help=u'待分期金额，单位元')
+    parser.add_argument(
+        'stages', type=int,
+        help=u'分期数')
+    parser.add_argument(
+        'returns', type=float,
+        help=u'年化收益率，单位%')
+    parser.add_argument(
+        'fee', type=float, default=0, nargs='?',
+        help=u'月分期费率，单位%，默认为0')
+    return parser
